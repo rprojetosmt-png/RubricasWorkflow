584a8d7c80c920c529ab4365bd304d102d02fb74
Renan Araujo <rprojetosmt@gmail.com>
2026-05-06 10:04:04 -0400

    chore: generate Vite dependency pre-bundling artifacts

---
+++ .vite/deps/@radix-ui_react-slot.js
@@ -0,0 +1,13 @@
+import {
+  Root,
+  Slot,
+  Slottable
+} from "./chunk-OWDJPS5H.js";
+import "./chunk-RZSASJON.js";
+import "./chunk-TWJRYSII.js";
+import "./chunk-DC5AMYBS.js";
+export {
+  Root,
+  Slot,
+  Slottable
+};
